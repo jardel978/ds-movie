1098be5dfd9da5215a99c6e7f1e5302106a82b5a
jardel978 <jardeloliver197@gmail.com>
2022-01-12 20:48:01 -0300

    MovieCard

---
+++ frontend/src/components/MovieScore/index.tsx
@@ -0,0 +1,16 @@
+import './style.css';
+import { MovieStars } from 'components/MovieStars';
+
+export const MovieScore = () => {
+
+    const score = 3.5,
+        count = 13;
+
+    return (
+        <div className="dsmovie-score-container">
+            <p className="dsmovie-score-value">{score > 0 ? score.toFixed(1) : '-'}</p>
+            <MovieStars />
+            <p className="dsmovie-score-count">{count} avaliações</p>
+        </div>
+    )
+}
