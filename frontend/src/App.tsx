8914b447084d3f7fcc2df3d6c6848a53ea195e25
jardel978 <jardeloliver197@gmail.com>
2022-01-12 19:58:56 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
 import { Navbar } from "./components/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Listing } from "pages/Listing";
+import { Form } from "pages/Form";
 
 function App() {
   return (
-    <>
-      <Navbar />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path=":movieId" element={<Form />} />
+        <Navbar />
+
+      </Routes>
+    </BrowserRouter>
   );
 }
 
