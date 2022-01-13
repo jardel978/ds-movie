dbc95d5dc7b89df01a54ec9a5a8e5bc848442204
Jardel Oliveira <jardeloliver197@gmail.com>
2022-01-13 17:32:40 -0300

    Update index.tsx

--- frontend/src/components/Pagination/index.tsx
+++ frontend/src/components/Pagination/index.tsx
@@ -1,16 +1,16 @@
 import './style.css';
-import { ReactComponent as Arrow } from '../../assets/img/arrow.svg';
+//import { ReactComponent as Arrow } from '../../assets/img/arrow.svg';
 
 export const Pagination = () => {
     return (
         <div className="dsmovie-pagination-container">
             <div className="dsmovie-pagination-box">
                 <button className="dsmovie-pagination-button" disabled={true} >
-                    <Arrow  className="dsmovie-flip-horizontal" />
+                   // <Arrow  className="dsmovie-flip-horizontal" />
                 </button>
                 <p>{`${1} de ${3}`}</p>
                 <button className="dsmovie-pagination-button" disabled={false} >
-                    <Arrow />
+                //    <Arrow />
                 </button>
             </div>
         </div>
